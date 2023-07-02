288a119a359930782e029c813cf52f9f51cdb0dd
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-07-02 12:38:01 +0530

    2 tabs done

--- src/pages/blog-posts/list.tsx
+++ src/pages/blog-posts/list.tsx
@@ -1,5 +1,5 @@
 import { IResourceComponentsProps } from "@refinedev/core";
-import { Button, Form, Input, Tabs } from "antd";
+import { Button, Form, Input, Tabs, Table } from "antd";
 import { useState, useRef, useEffect } from "react";
 import apiHelper from "utility/apiHelper";
 
@@ -8,6 +8,7 @@ const { TabPane } = Tabs;
 export const BlogPostList: React.FC<IResourceComponentsProps> = () => {
   const [formValues, setFormValues] = useState({});
   const [activeTab, setActiveTab] = useState("1");
+  const [contactDetails, setContactDetails] = useState([]);
 
   const handleNext = async () => {
     try {
@@ -26,36 +27,9 @@ export const BlogPostList: React.FC<IResourceComponentsProps> = () => {
       try {
         const response = await apiHelper('get', '/company', null);
         const { data } = response;
-        
-        formRef.current.setFieldsValue({
-          about: {
-            name: data.about.name,
-            description: data.about.description,
-          },
-          contactDetails: data.contactDetails.map((contact:any) => ({
-            name: contact.name,
-            email: contact.email,
-          })),
-          legalInformation: data.legalInformation.map((info:any) => ({
-            companyType: info.companyType,
-            registrationNumber: info.registrationNumber,
-          })),
-          offices: data.offices.map((office:any) => ({
-            address: [
-              {
-                line1: office.address[0].line1,
-                city: office.address[0].city,
-              },
-            ],
-            contact: [
-              {
-                phoneNumber: office.contact[0].phoneNumber,
-                email: office.contact[0].email,
-              },
-            ],
-          })),
-        });
-        
+
+        formRef.current.setFieldsValue(data);
+        setContactDetails(data.contactDetails);
         setFormValues(data);
       } catch (error) {
         console.log("Error fetching data:", error);
@@ -76,6 +50,28 @@ export const BlogPostList: React.FC<IResourceComponentsProps> = () => {
 
   const formRef = useRef<any>(null);
 
+  const columns = [
+    { title: 'Title', dataIndex: 'title', key: 'title' },
+    { title: 'Name', dataIndex: 'name', key: 'name' },
+    { title: 'Phone Number', dataIndex: 'phoneNumber', key: 'phoneNumber' },
+    { title: 'Email', dataIndex: 'email', key: 'email' },
+    { title: 'Designation', dataIndex: 'designation', key: 'designation' },
+    { title: 'Company', dataIndex: 'company', key: 'company' },
+    { title: 'Created At', dataIndex: 'createdAt', key: 'createdAt' },
+    { title: 'Updated At', dataIndex: 'updatedAt', key: 'updatedAt' },
+    {
+      title: 'Actions',
+      dataIndex: '_id',
+      key: 'actions',
+      render: (id: string) => <Button onClick={() => handleDelete(id)}>Delete</Button>,
+    },
+  ];
+
+  const handleDelete = (id: string) => {
+    // Perform delete operation for the given id
+    console.log('Delete contact with ID:', id);
+  };
+
   return (
     <Form
       ref={formRef}
@@ -85,9 +81,16 @@ export const BlogPostList: React.FC<IResourceComponentsProps> = () => {
       <Tabs activeKey={activeTab} onChange={setActiveTab}>
         <TabPane tab="About" key="1">
           <Form.Item
-            label="Name"
+            label="Company Name"
             name={['about', 'name']}
-            rules={[{ required: true, message: 'Name is required' }]}
+            rules={[{ required: true, message: 'Company Name is required' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Industry"
+            name={['about', 'industry']}
+            rules={[{ required: true, message: 'Industry is required' }]}
           >
             <Input />
           </Form.Item>
@@ -98,53 +101,117 @@ export const BlogPostList: React.FC<IResourceComponentsProps> = () => {
           >
             <Input.TextArea />
           </Form.Item>
+          <Form.Item
+            label="Website"
+            name={['about', 'website']}
+            rules={[
+              { required: true, message: 'Website is required' },
+              { type: 'url', message: 'Invalid URL' },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Employee Strength"
+            name={['about', 'employeeStrength']}
+            rules={[{ required: true, message: 'Employee Strength is required' }]}
+          >
+            <Input type="number" />
+          </Form.Item>
           <Button type="primary" onClick={handleNext}>
             Next
           </Button>
         </TabPane>
         <TabPane tab="Contact Details" key="2">
-          <Form.Item
-            label="Name"
-            name={['contactDetails', 0, 'name']}
-            rules={[{ required: true, message: 'Name is required' }]}
-          >
-            <Input />
+          <Table
+            dataSource={contactDetails}
+            columns={columns}
+            pagination={false}
+          />
+          <Form.Item>
+            <Button onClick={handlePrev}>Previous</Button>
+            <Button type="primary" onClick={handleNext}>
+              Next
+            </Button>
           </Form.Item>
-          <Form.Item
-            label="Email"
-            name={['contactDetails', 0, 'email']}
-            rules={[
-              { required: true, message: 'Email is required' },
-              { type: 'email', message: 'Invalid email' },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Button onClick={handlePrev}>Previous</Button>
-          <Button type="primary" onClick={handleNext}>
-            Next
-          </Button>
         </TabPane>
         <TabPane tab="Legal Information" key="3">
-          <Form.Item
-            label="Company Type"
-            name={['legalInformation', 'companyType']}
-            rules={[{ required: true, message: 'Company type is required' }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Registration Number"
-            name={['legalInformation', 'registrationNumber']}
-            rules={[{ required: true, message: 'Registration number is required' }]}
-          >
-            <Input />
-          </Form.Item>
-          <Button onClick={handlePrev}>Previous</Button>
-          <Button type="primary" onClick={handleNext}>
-            Next
-          </Button>
-        </TabPane>
+  <Form.Item
+    label="Company Type"
+    name={['legalInformation', 'companyType']}
+    rules={[{ required: true, message: 'Company type is required' }]}
+  >
+    <Input />
+  </Form.Item>
+  <Form.Item
+    label="Registration Number"
+    name={['legalInformation', 'registrationNumber']}
+    rules={[{ required: true, message: 'Registration number is required' }]}
+  >
+    <Input />
+  </Form.Item>
+  <Form.Item
+    label="Year of Registration"
+    name={['legalInformation', 'yearOfRegistration']}
+    rules={[{ required: true, message: 'Year of registration is required' }]}
+  >
+    <Input type="number" />
+  </Form.Item>
+  <Form.Item
+    label="TIN"
+    name={['legalInformation', 'tin']}
+    rules={[{ required: true, message: 'TIN is required' }]}
+  >
+    <Input />
+  </Form.Item>
+  <Form.Item
+    label="Last Financial Year Turnover"
+    name={['legalInformation', 'lastFinancialYearTurnover']}
+    rules={[{ required: true, message: 'Last financial year turnover is required' }]}
+  >
+    <Input type="number" />
+  </Form.Item>
+  <Form.Item
+    label="Registration Address"
+    name={['legalInformation', 'registrationAddress', 'line1']}
+    rules={[{ required: true, message: 'Registration address line 1 is required' }]}
+  >
+    <Input />
+  </Form.Item>
+  <Form.Item
+    label="Registration Address Line 2"
+    name={['legalInformation', 'registrationAddress', 'line2']}
+  >
+    <Input />
+  </Form.Item>
+  <Form.Item
+    label="City"
+    name={['legalInformation', 'registrationAddress', 'city']}
+    rules={[{ required: true, message: 'City is required' }]}
+  >
+    <Input />
+  </Form.Item>
+  <Form.Item
+    label="State"
+    name={['legalInformation', 'registrationAddress', 'state']}
+    rules={[{ required: true, message: 'State is required' }]}
+  >
+    <Input />
+  </Form.Item>
+  <Form.Item
+    label="Pincode"
+    name={['legalInformation', 'registrationAddress', 'pincode']}
+    rules={[{ required: true, message: 'Pincode is required' }]}
+  >
+    <Input />
+  </Form.Item>
+  <Button onClick={handlePrev}>Previous</Button>
+  <Button type="primary" onClick={handleNext}>
+    Next
+  </Button>
+</TabPane>
+
+
         <TabPane tab="Offices" key="4">
           <Form.Item
             label="Address Line 1"
